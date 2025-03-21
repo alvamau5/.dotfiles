72b466b059985a038ad4aedbc64c2e0bf8a05d84
alvamau5 <lucioaalvaradomolina@outlook.com>
2025-03-21 01:27:22 -0600

    update alpha.lua

--- nvim/lua/plugings/alpha.lua
+++ nvim/lua/plugings/alpha.lua
@@ -6,7 +6,7 @@ return {
 
   config = function()
     local alpha = require("alpha")
-    local dashboard = require("alpha.themes.startify")
+    local dashboard = require("alpha.themes.dashboard")
 
     dashboard.section.header.val = {
       [[                                                                       ]],
@@ -26,6 +26,23 @@ return {
       [[                                                                       ]],
     }
 
-    alpha.setup(dashboard.opts)
+	dashboard.section.buttons.val = {
+	dashboard.button("f", "󰈞 Find file", ":Telescope find_files<CR>"),
+	dashboard.button("n", " New file", ":ene <BAR> startinsert<CR>"),
+	dashboard.button("r", "󰥔 Recent files", ":Telescope oldfiles<CR>"),
+	dashboard.button("c", "  Config", ":e $MYVIMRC<CR>"),
+  	dashboard.button("q", " Quit", ":qa<CR>"),
+	}
+	dashboard.section.footer.val = "By Darlingi 󰣇"
+
+	alpha.setup(dashboard.opts)
+
+
+	vim.api.nvim_set_hl(0, 'DashboardHeader', { fg = "#7BA2F6", bg = "NONE" })
+	vim.api.nvim_set_hl(0, 'DashboardCenter', { fg = "#7BA2F6", bg = "NONE" })
+	vim.api.nvim_set_hl(0, 'DashboardFooter', { fg = "#7BA2F6", bg = "NONE" })
+
+
+	vim.opt.termguicolors = true
   end,
 }
