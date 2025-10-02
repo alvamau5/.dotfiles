a7712e00610bf20c55d854e4e480c4a7ca57cd1f
Lucio Antonio Alvarado Molina <89697398+alvamau5@users.noreply.github.com>
2025-10-02 00:00:25 -0600

    Update install.sh
    Change all configuration

--- install.sh
+++ install.sh
@@ -1,33 +1,15 @@
 #!/usr/bin/env bash
-#
-set -e
 
-DISTRO=$(grep -E "^NAME" /etc/os-release)
-DISTRO_NAME=""
+# Function to keep updating the sudo timestamp until the script ends
+keep_sudo_alive() {
+    while true; do sudo -n true; sleep 60; done 2>/dev/null &
+}
 
-echo "Installing oh-my-zsh and, neovim and devtools in Linux distro like fedora, ubuntu"
-#
-echo "detecting the Linux distribution"
-# NAME="Fedora Linux"
-# NAME="Ubuntu"
-case "$DISTRO" in
-  "NAME=\"Fedora Linux\"")
-    # echo "fedora"
-    DISTRO_NAME="fedora"
-    ;;
-  "NAME=\"Ubuntu\"")
-    # echo "ubuntu"
-    DISTRO_NAME="ubuntu"
-    ;;
-esac
-
-if [ $DISTRO_NAME = "fedora" ]; then
-   #sudo dnf upgrade -y
-    sudo dnf install make zsh curl wget @development-tools -y
-elif [ $DISTRO_NAME = "ubuntu" ]; then
-    sudo apt update && sudo apt upgrade -y
-    sudo apt install zsh curl wget build-essential git -y
-fi
+# Function to check if a command exists
+check_command() {
+    local cmd="$1"
+    command -v "$cmd" &> /dev/null
+}
 
 install_brew() {
   if ! check_command brew; then
@@ -40,75 +22,136 @@ install_brew() {
   fi
 }
 
-echo "Installing dependencies"
-# Installing dependencies
-brew install neovim
-brew install node
-brew install npm
+create_symlinks() {
+    echo "Removing existing dotfiles..."
+    rm -rf ~/.vim ~/.vimrc ~/.zshrc ~/.config/nvim ~/.ideavimrc ~/.wezterm.lua ~/.config/starship.toml	2>/dev/null
 
-echo "Creating symlinks Neovim..."
-# Neovim expects some folders already exist
-mkdir -p ~/.config/ ~/.config/nvim/
+    echo "Creating symlinks..."
+    mkdir -p ~/projects ~/.config
 
-# Symlinking files neovim
-ln -srv ~/.dotfiles/nvim/* ~/.config/nvim/
+    ln -s ~/.dotfiles/zshrc ~/.zshrc
+    ln -s ~/.dotfiles/nvim ~/.config/nvim
+    ln -s ~/.dotfiles/ghostty ~/.config/ghostty
+    ln -s ~/.dotfiles/yazi ~/.config/yazi.toml
 
-# Install terminal Kitty
-echo "Installing Terminal Kitty..."
-sudo dnf install -y kitty
+}
 
-# Kitty expects some folders already exist
-mkdir -p ~/.config/ ~/.config/kitty/
+install_brew_packages() {
+echo "Installing packages brew"
+	brew update
 
-echo "Creating symlinks Kitty..."
-# Symlinking files terminal kitty
-ln -srv ~/.dotfiles/kitty/* ~/.config/kitty/
+     	brew install neovim
+     	brew install nvm   
+     	brew install node
+     	brew install npm
+     	brew install gh
+     	brew install starship
+	brew install zsh-autosuggestions
+	brew install zsh-syntax-highlighting
+      	brew install zsh-completions
+      	brew install yazi ffmpegthumbnailer ffmpeg sevenzip jq poppler fd ripgrep fzf zoxide imagemagick font-symbols-only-nerd-font
 
-# Install ranger file manager
-echo "Install Ranger File Manager and Configs..."
-sudo dnf install -y ranger
-sudo dnf install -y w3m w3m-img
+	if ! check_command fzf; then
+		brew install fzf
+		# Add FZF shortcuts
+		"$(brew --prefix)"/opt/fzf/install
+	fi
+}
 
-echo "Creating folder for ranger"
-# Create folder for ranger file manager
-mkdir -p ~/.config/ ~/.config/ranger/
+install_brew_cask_packages() {
+	brew install --cask whatsapp
+	brew install --cask ghostty
+}
 
-echo "Creating symlinks ranger files"
-# Symlinking files terminal kitty
-ln -srv ~/.dotfiles/ranger/* ~/.config/ranger/
+setup_linux() {
+	echo -e "Using specific config for Linux \n"
 
-echo "Installing promt Starship"
-curl -sS https://starship.rs/install.sh | sh
+	# update OS
+	# sudo dnf upgrade -y
 
-echo "Removing existing dotfiles"
-# remove files if they already exist
-rm -rf ~/.zshrc
+	sudo dnf install -y zsh curl wget git
+	sudo dnf5 install @development-tools -y
 
-echo "Creating symlinks ZSH"
-# Symlinking files zsh
-ln -srv ~/.dotfiles/zshrc ~/.zshrc
+	create_symlinks
 
-#Install plugins in zsh
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+	# Install Fonts
+	mkdir -p ~/.local/share/fonts
+	echo "Installing Cascadia"
+	wget -qO- $(curl -s https://api.github.com/repos/microsoft/cascadia-code/releases/latest | grep browser_download_url | grep zip | cut -d '"' -f 4) -O cascadia.zip
+	unzip -o cascadia.zip -d ~/.local/share/fonts
+	rm cascadia.zip
+	fc-cache -fv
 
-echo "Installing Oh My ZSH"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	install_brew
+	install_starship
+	install_brew_packages
 
-# Check if the current shell is already zsh
-if [[ "$SHELL" == *"zsh" ]]; then
-    echo "ZSH is the default shell."
+	# Check if the current shell is already zsh
+	if [[ "$SHELL" == *"zsh" ]]; then
+	    echo "ZSH is the default shell."
+	else
+	    # Get the path of zsh
+	    zsh_path=$(which zsh)
+
+	    # Change the default shell to zsh for future logins
+	    echo "Setting up zsh as your default shell..."
+	    if chsh -s "$zsh_path"; then
+		echo "Setup complete. Log out and back in to start using zsh as your default shell."
+	    else
+		echo "Error: Failed to change the default shell."
+		echo "Please try running 'chsh -s $(which zsh)' manually."
+	    fi
+	fi
+}
+
+setup_mac() {
+	echo -e "Using specific config for Mac \n"
+
+	create_symlinks
+
+	install_brew
+
+	brew tap homebrew/cask-fonts
+
+	# casks only work in mac
+	echo "Installing Caskaydia"
+        brew install --cask font-caskaydia-cove-nerd-font
+
+	brew install reattach-to-user-namespace
+
+	install_brew_packages
+	install_brew_cask_packages
+}
+
+os_type=""
+
+if check_command ujust; then
+    os_type="Bluefin"
+elif [[ $(uname) == "Darwin" ]]; then
+    os_type="Mac"
 else
-    # Get the path of zsh
-    zsh_path=$(which zsh)
-
-    # Change the default shell to zsh for future logins
-    echo "Setting up zsh as your default shell..."
-    if chsh -s "$zsh_path"; then
-	echo "Setup complete. Log out and back in to start using zsh as your default shell."
-    else
-	echo "Error: Failed to change the default shell."
-	echo "Please try running 'chsh -s $(which zsh)' manually."
-    fi
+    os_type="Linux"
 fi
+
+echo -e "$os_type detected. Using $os_type config... \n"
+
+# Ask for the administrator password upfront and keep the sudo timestamp updated
+#sudo -v
+
+# keep_sudo_alive
+
+case $os_type inee
+    "Bluefin")
+        setup_bluefin
+        ;;
+    "Mac")
+        setup_mac
+        ;;
+    "Linux")
+        setup_linux
+        ;;
+    *)
+        echo "Unknown OS type: $os_type. Exiting."
+        exit 1
+        ;;
+esac
